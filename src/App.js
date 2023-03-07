991c79878c76d4b4ad88ed9ec9b5d13609893e12
Darius Loftis <dloft9@gmail.com>
2023-03-06 16:13:44 -0800

    new initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import "./App.scss";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div className="App" data-test-id="app">
-      <h2>Run: npm run storybook</h2>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
